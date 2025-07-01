c1cff6325cf15137f93e4930012710189bbff87c
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-07-01 07:39:17 +0600

    Index js  ✈

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
-
+// index js
 const app = express();
 const port = 5000;
 
